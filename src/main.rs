238daf7819f2ba03ebd51acb365d861e88cb7c81
Michael E. Craggs <m.e.craggs@gmail.com>
2019-01-23 20:01:36 +0100

    Add main binary to expose helpers for testing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 extern crate mrpump;
 extern crate rustls;
-mod common;
 
 use mrpump::{spawn_server, util::rcgen_self_signed, AccessControl};
 use rustls::{NoClientAuth, ServerConfig};
+use std::net::SocketAddr;
 
-#[test]
-fn server_already_bound() {
-    let addr = common::get_loopback_v4_sock_addr_base();
-    let alt_names = common::get_subject_alt_names();
+fn main() {
+    let addr = "0.0.0.0:7337".parse::<SocketAddr>().unwrap();
+    let alt_names = vec!["test".to_string(), "localhost".to_string()];
     let (cert, key) = rcgen_self_signed(alt_names).unwrap();
     let mut config = ServerConfig::new(NoClientAuth::new());
     config.set_single_cert(vec![cert], key).unwrap();
