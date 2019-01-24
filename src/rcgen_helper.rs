391abd2b0808c89eba54668d559a4963f0b4ba79
Michael E. Craggs <m.e.craggs@gmail.com>
2019-01-24 18:58:30 +0100

    Debugger support

--- src/rcgen_helper.rs
+++ src/rcgen_helper.rs
@@ -10,21 +10,37 @@ pub fn rcgen_self_signed(
 ) -> Result<(Certificate, PrivateKey), Error> {
     let x509 = generate_simple_self_signed(subject_alt_names);
     let cert = x509.serialize_pem();
+    println!("{}", cert);
     let mut cert = BufReader::new(cert.as_bytes());
     let mut certs = load_certs_wrapped("rcgen", &mut cert)?;
     let cert = certs.remove(0);
 
     let key = x509.serialize_private_key_pem();
+    println!("{}", key);
     let mut key = BufReader::new(key.as_bytes());
     let key_rsa = match rustls::internal::pemfile::rsa_private_keys(&mut key) {
-        Ok(keys) => Some(keys[0].clone()),
+        Ok(keys) => {
+            if keys.is_empty() {
+                None
+            } else {
+                Some(keys[0].clone())
+            }
+        }
         Err(_) => None,
     };
 
     let key_pkcs8 = match rustls::internal::pemfile::pkcs8_private_keys(&mut key) {
-        Ok(keys) => Some(keys[0].clone()),
+        Ok(keys) => {
+            if keys.is_empty() {
+                None
+            } else {
+                Some(keys[0].clone())
+            }
+        }
         Err(_) => None,
     };
+
+    println!("rsa: {:?}\npkcs8: {:?}", key_rsa, key_pkcs8);
 
     if key_rsa.is_some() {
         return Ok((cert, key_rsa.unwrap()));
